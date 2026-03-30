d51a6fa8bac0c822c80dcf376b81bb8d06fb6aac
ketankhandelwal <ketankhandelwal70@gmail.com>
2026-03-30 14:30:04 +0530

    Fix in vite

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,25 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc"
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  base: "/", 
+export default defineConfig({
+  base: "/",
   server: {
     host: true,
     port: 8080,
     strictPort: true,
-
-
-    // allowedHosts: [
-    //   "wholesome-sheila-piddling.ngrok-free.dev"
-    // ],
-
     fs: {
-      strict: false 
-    }
+      strict: false,
+    },
   },
-  plugins: [
-    react(),
-    mode === 'development'
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(import.meta.dirname, "./src"),
     },
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
   },
-}));
+});
